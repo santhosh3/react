1f410784784b245711638b372c9542821149476c
santhosh3 <santhoshchinna109@gmail.com>
2022-08-21 14:32:08 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
-import React, { useMemo, useState } from "react";
-const choices = ["rock", "paper", "scissors"];
-
-export default function App() {
-  const [selected, setSelected] = useState("");
-  const [computedSelected, setComputedSelected] = useState("");
-
-  const play = () => {
-    if (!selected) {
-      return;
-    }
-    const computerChoiceIndex = Math.floor(Math.random() * choices.length);
-    setComputedSelected(choices[computerChoiceIndex]);
-  };
-
-  const result = useMemo(() => {
-    if (computedSelected === selected) {
-      return `it's a tie`;
-    } else {
-      if (
-        (computedSelected === "rock" && selected === "scissors") ||
-        (computedSelected === "paper" && selected === "rock") ||
-        (computedSelected === "scissors" && selected === "paper")
-      ) {
-        return "player2 won";
-      }
-      return "player1 won";
-    }
-  }, [computedSelected, selected]);
-
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+function App(){
+  const [data,setData] = useState([])
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then(
+      response => setData(response.data)
+    )
+  },[])
   return (
     <div>
-      <center>
-      <div>
-        <button onClick={() => setSelected("rock")}>rock</button>
-        <button onClick={() => setSelected("paper")}>paper</button>
-        <button onClick={() => setSelected("scissors")}>scissors</button>
-      </div>
-      <button onClick={play}>play</button>
-      <p>player1 choice: {selected}</p>
-      <p>player2 choice: {computedSelected}</p>
-      <div>{result}</div>
-      </center>
+      {data.map(items => <li key = {items.id}>{items.title}</li>)}
     </div>
-  );
-}
+  )
 
+}
+export default App
