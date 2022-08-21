ad2abb797f27e7c6f324432dcd6b777ed6de3b23
santhosh3 <santhoshchinna109@gmail.com>
2022-08-21 20:58:06 +0530

    image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
-import Navbar from './Navbar'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import Home from './Home'
-import About from './about'
-import Dashboard from './Dashboard';
+import image from './image.jpeg';
 function App(){
   return (
-    <div>
-      <h1>CREATING MULTIPAGE APPLICATIONS</h1>
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path = "/Home" element = {<Home/>} />
-        <Route path = "/About" element = {<About/>} />
-        <Route path = "/Dashboard/:name" element = {<Dashboard/>} />
-      </Routes>
-      </BrowserRouter>
-    </div>
+   <div>
+    <center>
+      <img src = {image} height = "670" width="auto" alt="photo" />
+    </center>
+   </div>
   )
 
 }
