a9de954767812ac3349a0e96f648d2fef45bd69d
santhosh3 <santhoshchinna109@gmail.com>
2022-08-27 14:02:17 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React,{useState} from "react";
+import Img from "./img";
 import './App.css';
 
 function App() {
-  const [data,setData] = useState({
-    email : "",
-    password : ""
-  })
-  const changeHandler = e => {
-    setData({...data,[e.target.name]:e.target.value})
-  }
-  const submitHandler = e => {
-    e.preventDefault();
-    if(data.password.length < 5){
-      alert("password length must be greater than 5")
-    }else{
-      console.log(data)
-    }
-  }
+  const arr = [
+    "https://images.unsplash.com/photo-1546182990-dffeafbe841d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YW5pbWFsfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    "https://cdn.pixabay.com/photo/2018/08/12/16/59/parrot-3601194_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2014/09/16/20/52/gorilla-448731__340.jpg"
+  ]  
   return (
-    <div className="App">
-        <form onSubmit={submitHandler}>
-          <label style={{"color":"blue","fontFamily" :"Tahoma","flexDirection":"inline"}}>E-mail</label><br />
-          <input type='text' name="email" onChange={changeHandler} placeholder='email' /><br />
-          <label >password</label><br />
-          <input type='password' name="password" onChange={changeHandler} placeholder='password' /><br /><br />
-          <input type="submit" value="login" className="btn btn-primary"/><br />
-        </form>
+    <div style={{display:"flex"}}>
+      {arr.map((item,index) => <Img key = {index} src={item} />)}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/img.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const img = (props) => {
+  return (
+    <div>
+      <img src={props.src} height={150} width={150} />
+    </div>
+  )
+}
+
+export default img
+
