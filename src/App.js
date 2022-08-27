ed30c2f827fbb30cb1d8917a80c44ce414a85344
santhosh3 <santhoshchinna109@gmail.com>
2022-08-27 13:38:52 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React,{useState} from "react";
-import Text from "./test";
+import './App.css';
 
 function App() {
   const [data,setData] = useState({
-    name : "raju",
-    Age : 23
+    email : "",
+    password : ""
   })
-  const {name,Age} = data
+  const changeHandler = e => {
+    setData({...data,[e.target.name]:e.target.value})
+  }
+  const submitHandler = e => {
+    e.preventDefault();
+    if(data.password.length < 5){
+      alert("password length must be greater than 5")
+    }else{
+      console.log(data)
+    }
+  }
   return (
-    <div>
-      <center>
-        <Text name = {name} and Age = {Age}/>
-      </center>
+    <div className="App">
+        <form onSubmit={submitHandler}>
+          <label style={{"color":"blue","fontFamily" :"Tahoma","flexDirection":"inline"}}>E-mail</label><br />
+          <input type='text' name="email" onChange={changeHandler} placeholder='email' /><br />
+          <label >password</label><br />
+          <input type='password' name="password" onChange={changeHandler} placeholder='password' /><br /><br />
+          <input type="submit" value="login" className="btn btn-primary"/><br />
+        </form>
     </div>
   );
 }
