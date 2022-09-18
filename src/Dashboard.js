f55f6ee64670d6ffa9531c9649a5b1f375a0ea88
santhosh3 <santhoshchinna109@gmail.com>
2022-09-18 18:56:15 +0530

    pathParams

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './INDEX.css';
+import { useParams} from 'react-router-dom'
 
 function dashboard() {
+ let params = useParams()
   return (
     <div>
       <center>
         <h3>Welcome to dashboard page</h3>
+        <p> Name : {params.name}</p>
         <Link to='/' className='Link'>Home page</Link>
         <Link to='/about' className='Link'>About</Link>
       </center>
--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom'
 import './INDEX.css';
 
 function home() {
+  const user = 'John kumar Reddy'
   return (
     <div>
       <center>
         <h3>Welcome to Home page</h3>
-        <Link to='/dashboard' className='Link'>Dashboard</Link>
+        <Link to={`/dashboard/${user}`} className='Link'>Dashboard</Link>
         <Link to='/about' className='Link'>About</Link>
       </center>
     </div>
