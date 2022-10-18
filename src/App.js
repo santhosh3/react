07f8fa3be6500046f6dd2221ff720acf60f906ab
santhosh3 <santhoshchinna109@gmail.com>
2022-10-18 10:43:40 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// const App = () => {
+//   const names = ["Bruce", "Clark", "Rahul"]
+//   return (
+//    <center>
+//      <div>
+//       {
+//         names.map((name,index) => {return <h2 key={index}>{name}</h2>})
+//       }
+//     </div>
+//    </center>
+//   )
+// }
+
+// export default App
+
+// import React,{useState} from 'react'
+// const App = () => {
+//   const [userName, setuserName] = useState('')
+
+//   const handlerSubmit = (event) => {
+//     event.preventDefault();
+//     alert(`Form data is ${userName}`)
+//   }
+//   return (
+//     <form onSubmit={handlerSubmit}>
+//       <div>
+//       <label>userName</label>
+//       <input 
+//         type='text'
+//         value={userName}
+//         onChange={(event) => setuserName(event.target.value)}
+//         />
+//       </div>
+//       <button type='submit'>submit</button>
+//     </form>
+//   )
+// }
+//export default App
+
+import React,{useState,useEffect} from "react";
+import './App.css'
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((data) => setPosts(data))
+    .catch(err => console.log(err))
+  },[]);
+
+  return(
+    <div className="primary">
+      <ul>
+        {
+          posts.map((post) => {
+            return <h1><li key = {post.id}>{post.body}</li></h1>
+          })
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
